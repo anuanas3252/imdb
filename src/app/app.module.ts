ead7307d19d6bd777545556243d548fc5b1e257a
anuanas3252 <anuanas3252@gmail.com>
2021-02-25 23:48:04 +0530

    Added third party modules.
    Added ngx pagination.
    Added Ng bootstrap for staring in mobile view.
    Added applicationstate service to detect the desktop and mobile.
    Added ServiceWorkerModule and environment for caching purposes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,33 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DesktopviewComponent } from './desktopview/desktopview.component';
+import { MobileviewComponent } from './mobileview/mobileview.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConverterPipe
+    ConverterPipe,
+    DesktopviewComponent,
+    MobileviewComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AppRoutingModule,
+
+    //third-party modules
+    NgxPaginationModule,
+    NgbModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+
 
   ],
   providers: [],
